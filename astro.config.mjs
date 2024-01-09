6284dc91bb23554fa629cf6f84db7e34b57670cd
Waleed Sadek <62147992+waleedsadek-panx@users.noreply.github.com>
2024-01-09 07:11:09 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://xposure.panx.io',
+  site: 'https://panx.io',
 });
